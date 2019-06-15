20d3145d5fe9ffff282fc45f01fd507f4c8d0b82
Steven Xu <xhrsteven@gmail.com>
2019-06-15 10:14:08 +0800

    Update

--- wordcount.py
+++ wordcount.py
@@ -1,12 +1,12 @@
+
 import sys
 # import os
-#
 # os.environ['JAVA_HOME'] = 'D:\Program Files\Java\jdk1.8.0_144'
 from pyspark import SparkConf, SparkContext
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: wordcount<input> ", file=sys.stderr)
+    if len(sys.argv) != 3:
+        print("Usage: wordcount<input> <output>", file=sys.stderr)
         sys.exit(-1)
 
     conf = SparkConf()
@@ -22,15 +22,16 @@ if __name__ == '__main__':
         output = counts.collect()
         for (word, count) in output:
             print("%s ： %i" % (word, count))
-    printresult()
 
-    # def saveFile():
-    #     sc.textFile(sys.argv[1]) \
-    #         .flatMap(lambda line: line.split("\t")) \
-    #         .map(lambda x: (x, 1)) \
-    #         .reduceByKey(lambda a, b: a + b) \
-    #         .saveAsFile(sys.argv[2])
-    #
-    # saveFile()
+    def saveFile():
+        sc.textFile(sys.argv[1]) \
+            .flatMap(lambda line: line.split("\t")) \
+            .map(lambda x: (x, 1)) \
+            .reduceByKey(lambda a, b: a + b) \
+            .saveAsTextFile(sys.argv[2])
+
+    saveFile()
 
     sc.stop()
+
+#./spark-submit --master local[2] --name spark0301 /opt/module/spark-2.4.3-bin-hadoop2.7/examples/spark0301.py file:///opt/module/spark-2.4.3-bin-hadoop2.7/data/wc file:///opt/module/spark-2.4.3-bin-hadoop2.7/data/tmp
